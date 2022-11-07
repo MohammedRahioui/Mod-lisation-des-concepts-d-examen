99c0c197f2d8d9dcd4ea43ccaf4adc8b282e2d90
HP <HP@DESKTOP-POLSQSD>
2022-11-07 09:44:00 -1200

    Add project files.

---
+++ Modélisation des concepts d'examen/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+
+namespace Modélisation
+{
+    class Program
+    {
+        public class Exam
+        {
+            private int IdE;
+            private int Note;
+            private DateTime DateDebut;
+            private DateTime DateFin;
+
+        }
+        public class Questions
+        {
+            private int Idq;
+            private int NbrQuestions;
+            private int Cne;
+
+        }
+
+        public class Etudiant
+        {
+            private int IdEtudiant;
+            private String Nom;
+            private String Prenom;
+            private int Cne;
+            private int Attribut5;
+
+        }
+        public class QuestionsOuvertes
+        {
+            private int Aucune;
+            private int PlusieursBonneReponses;
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
